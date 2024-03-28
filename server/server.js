ec8682efee29edd9bbbd24e3b086b27a081de569
CiamalaKombeGreg <he202098@students.ephec.be>
2024-03-28 11:09:18 +0100

    Commentaire rajouté #1

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require("express");
+//Pour demander/connecter la database
 require("./db");
+
+//Constantes de path pour les routes
 const UsersRouter = require("./routes/userRoute");
 const Login = require("./routes/login");
 const cors = require("cors");
 
 const app = express();
+
+//Route à utiliser
 app.use(cors());
 
 app.use(express.json());
 
+app.use(express.urlencoded({extended: false}));
+
 app.use(UsersRouter);
 app.use(Login);
 
+//Port d'écoute
 app.listen(8000, () => {
   console.log("Server listening on port 8000");
 });
