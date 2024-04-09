1b4b7432119fc7e65bc37820b778438962ac73d2
Dimitri19 <dimitri.meeus@hotmail.com>
2024-04-09 15:57:26 +0200

    fin conflit

--- server/controllers/gestionnaire.controller.js
+++ server/controllers/gestionnaire.controller.js
@@ -1,9 +1,7 @@
 //Variable lié à la table de models des membres
-<<<<<<< HEAD
-const Membres = require("../models/membres.models.js");
-=======
+
 const Membres = require("../models/membres.models");
->>>>>>> 0b9b6cc961151372f5793e2b90aaf835079197bd
+
 
 //Renvoie la liste des membres
 const getMembres = async (req, res) => {
