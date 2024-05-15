dc13aee695f49cbbf3f6faf940de103d60d27401
davidaddi <94124302+davidaddi@users.noreply.github.com>
2024-05-15 11:23:57 +0200

    CORS config correction

--- server/server.js
+++ server/server.js
@@ -5,7 +5,10 @@ const Login = require("./routes/login");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://rfc-wetteren-api.onrender.com/'],
+  credentials: true
+}));
 
 app.use(express.json());
 
