13c0b0f21d23ca7ba80f527972d1572670fe0a01
davidaddi <94124302+davidaddi@users.noreply.github.com>
2024-05-15 19:57:45 +0200

    correction bug CORS XMLHttpRequest

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-  origin: ['https://rfc-wetteren-api.onrender.com/'],
+  origin: ['https://rfc-wetteren.vercel.app/'],
   credentials: true
 }));
 
@@ -14,6 +14,13 @@ app.use(express.json());
 
 app.use(UsersRouter);
 app.use(Login);
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://rfc-wetteren.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.listen(8000, () => {
   console.log("Server listening on port 8000");
