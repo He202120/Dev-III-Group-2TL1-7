6b0e2781642269e3f02b8aafda35d336eaa09efe
Dimitri19 <dimitri.meeus@hotmail.com>
2024-05-15 23:44:52 +0200

    rajout dela navbar oublié dans agenda

--- client/src/pages/Agenda.jsx
+++ client/src/pages/Agenda.jsx
@@ -1,9 +1,13 @@
 import Liste_evenement from "../components/Agenda/liste_evenements";
-
+import NavBar from '@/components/NavBar';
 
 function Agenda(){
     return (
-        <Liste_evenement />
+        <>
+            <NavBar></NavBar>
+            <Liste_evenement />
+        </>
+        
     );
 }
 
