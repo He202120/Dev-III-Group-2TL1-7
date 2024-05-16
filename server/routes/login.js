0abec3a62b5a27ce02ab70bb0683447ce184ef96
davidaddi <94124302+davidaddi@users.noreply.github.com>
2024-05-16 19:41:56 +0200

    fix bugs

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,38 +7,26 @@ const jwt = require("jsonwebtoken");
 router.post("/login", async (req, res, next) => {
   try {
     const { email, password } = req.body;
-
-    // Debugging
-    console.log("Received email:", email);
-    console.log("Received password:", password);
-
     const user = await User.findOne({ email });
-
     if (!user) {
-      console.log("User not found");
-      return res.status(422).json({ error: "Identifiants incorrects" });
+      return res.status(422).json({ email: "Identifiants incorrects" });
     }
-
     const isPasswordValid = await bcrypt.compare(password, user.password);
-
     if (!isPasswordValid) {
-      console.log("Invalid password");
-      return res.status(422).json({ error: "Identifiants incorrects" });
+      return res.status(422).json({ email: "Identifiants incorrects" });
     }
-
     const token = jwt.sign({ sub: user._id, role: user.role }, process.env.JWTSECRET, {
       expiresIn: "1h",
     });
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
-      sameSite: "strict",
+      secure: true, 
+      sameSite: "strict"
     });
 
     res.json({ success: true });
   } catch (err) {
-    console.error("Error in login route:", err);
     res.status(500).json({ error: "Une erreur s'est produite lors de la connexion." });
   }
 });
